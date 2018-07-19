ea53b4e19d1ff0b12ec1c7337fbdb274e1b71baf
daiot <stefanescu.dragos@gmail.com>
2018-07-19 14:51:12 +0300

    initial release 1.0.0

---
+++ EnableSPAApiMiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace SpaApiMiddleware
+{
+    public static class EnableSPAApiMiddlewareExtensions
+    {
+         public static IApplicationBuilder UseSpaApiOnly(
+            this IApplicationBuilder builder, string apiPath="api", string indexHtmlPage="index.html")
+        {
+            return builder.UseMiddleware<EnableSPAApiMiddleware>(apiPath, indexHtmlPage);
+        }
+    }
+}
